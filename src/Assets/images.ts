7c45c457abda5583c64a5aba13462474e13abfb4
“Trishita12345” <trishita.majumder15@gmail.com>
2023-09-13 01:34:38 +0530

    home + about

---
+++ src/Assets/images.ts
@@ -0,0 +1,15 @@
+import myImage from './images/myImage.png';
+import Logo from './images/Logo.svg';
+import CrossIcon from './images/CrossIcon.svg'
+import BackgroundImage from './images/BackgroundImage.svg'
+import CornerImage from './images/CornerImage.svg'
+import BackgroundSection2 from './images/BackgroundSection2.png'
+
+export const images = {
+    myImage,
+    Logo,
+    CrossIcon,
+    CornerImage,
+    BackgroundImage,
+    BackgroundSection2
+}
---
+++ src/pages/MainContent.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import About from './About';
+import Home from './Home';
+
+const MainContent = () => {
+    return (
+        <>
+            <Home />
+            <About />
+        </>
+    );
+}
+
+export default MainContent;
